44197d0941aaff604ec70cfb6dd2a9b5517a6764
RoboDoig <erskine94@gmail.com>
2026-01-24 11:25:54 -0800

    Initial bonsai workflow

--- examples/rig.py
+++ examples/rig.py
@@ -6,7 +6,11 @@ from aind_behavior_dynamic_routing_bonsai.rig import (
     AindBehaviorDynamicRoutingBonsaiRig,
 )
 
-rig = AindBehaviorDynamicRoutingBonsaiRig(rig_name="test_rig")
+rig = AindBehaviorDynamicRoutingBonsaiRig(
+    rig_name="test_rig",
+    harp_behavior=rig.harp.HarpBehavior(port_name="COM1"),
+    harp_sound_card=rig.harp.HarpSoundCard(port_name="COM2")
+)
 
 def main(path_seed: str = "./local/{schema}.json"):
     os.makedirs(os.path.dirname(path_seed), exist_ok=True)
--- src/aind_behavior_dynamic_routing_bonsai/rig.py
+++ src/aind_behavior_dynamic_routing_bonsai/rig.py
@@ -1,11 +1,19 @@
 # Import core types
 from typing import Literal
+from pydantic import BaseModel, Field
 
 import aind_behavior_services.rig as rig
+import aind_behavior_services.calibration.water_valve as wvc
 
 from aind_behavior_dynamic_routing_bonsai import __semver__
 
+class RigCalibration(BaseModel):
+    water_valve: wvc.WaterValveCalibration = Field(..., description="Water valve calibration")
 
 class AindBehaviorDynamicRoutingBonsaiRig(rig.AindBehaviorRigModel):
     version: Literal[__semver__] = __semver__
-    ...
+    harp_behavior: rig.harp.HarpBehavior = Field(..., description="Harp behavior")
+    harp_sound_card: rig.harp.HarpSoundCard = Field(..., description="Harp sound card")
+    screen: rig.visual_stimulation.Screen = Field(
+        default=rig.visual_stimulation.Screen(), description="Screen settings"
+    )
